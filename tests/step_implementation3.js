e6242deace3441d2f83a67d6fdfb8c94a5ac605e
BalajiK <71222979+balajiregt@users.noreply.github.com>
2023-11-23 10:06:29 +0530

    Create step_implementation3.js

---
+++ tests/step_implementation3.js
@@ -0,0 +1,19 @@
+@Step("Start the application")
+public void startTheApplication() {
+    // Code to start the application
+}
+
+@Step("Enter username <username> and password <password>")
+public void enterCredentials(String username, String password) {
+    // Code to enter the username and password
+}
+
+@Step("Click the login button")
+public void clickLoginButton() {
+    // Code to click the login button
+}
+
+@Step("Verify the user is logged in")
+public void verifyUserLoggedIn() {
+    // Code to verify the user is logged in
+}
