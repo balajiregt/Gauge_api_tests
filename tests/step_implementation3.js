eac557efb75cdf66dba682a84de8f908dbf3b030
BalajiK <71222979+balajiregt@users.noreply.github.com>
2023-11-23 10:33:28 +0530

    Update step_implementation3.js

--- tests/step_implementation3.js
+++ tests/step_implementation3.js
@@ -1,16 +1,23 @@
+const axios = require('axios');
+const { expect } = require('chai');
+
 step("Start the application", async () => {
+    expect(true).to.be.true;
     console.log("First step")
 });
 
 step("Enter username and password", async () => {
+    expect(true).to.be.true;
     console.log("Second step")
 });
 
 step("Click the login button", async () => {
+    expect(true).to.be.true;
     console.log("Third step")
 });
 
 step("Verify the user is logged in", async () => {
+    expect(true).to.be.true;
     console.log("Fourth step")
 });
 
