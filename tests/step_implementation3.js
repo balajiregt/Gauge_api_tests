c0d99583fecd4ade10853f0876396e51a3d6e91b
BalajiK <71222979+balajiregt@users.noreply.github.com>
2023-11-23 10:42:05 +0530

    Update step_implementation3.js

--- tests/step_implementation3.js
+++ tests/step_implementation3.js
@@ -21,20 +21,12 @@ step("Verify the user is logged in", async () => {
     console.log("Fourth step")
 });
 
-tep("Start the application", async () => {
-    expect(true).to.be.true;
-    console.log("First step")
-});
 
 step("Enter incorrect username and password", async () => {
     expect(true).to.be.true;
     console.log("Second step")
 });
 
-step("Click the login button", async () => {
-    expect(true).to.be.true;
-    console.log("Third step")
-});
 
 step("Verify error message", async () => {
     expect(true).to.be.true;
